49f29383ef8a32ca141a7e2b7b3767f533ba1a99
Vladimir Aderikhin <v.aderikhin@roundlake.ru>
2013-01-22 16:49:43 +0400

    added support for main product image

--- app/liquid/tags/rails_tags.rb
+++ app/liquid/tags/rails_tags.rb
@@ -1,12 +1,17 @@
-#class FormAuthenticityTokenMenu < Liquid::Tag
-#  def initialize(tag_name, block, tokens)
-#    super
-#    @name = block
-#  end
-#
-#  def render(context)
-#    context.registers[:controller].form_authenticity_token.inspect
-#  end
-#end
-#
-#Liquid::Template.register_tag('form_authenticity_token', FormAuthenticityTokenMenu)
+class ImageTag < Liquid::Tag
+
+  def initialize(tag_name, markup, tokens)
+    unless markup.empty?
+      if markup =~ /url:([*.]+)/
+        @url = $1
+      end
+    end
+    super
+  end
+
+  def render(context)
+    context.registers[:action_view].image_tag(@url)
+  end
+end
+
+Liquid::Template.register_tag('image_tag', ImageTag)
