94b8fd73424008a020d40253879e26d9ed376efe
Alexander Negoda <alexander.negoda@gmail.com>
2013-02-22 02:14:52 +0400

    fix for locales

--- app/liquid/drops/order_drop.rb
+++ app/liquid/drops/order_drop.rb
@@ -1,6 +1,6 @@
 class Spree::OrderDrop < Clot::BaseDrop
 
-  self.liquid_attributes = [:id, :line_items, :ship_address, :bill_address, :user, :adjustments, :number, :email,
+  self.liquid_attributes = [:id, :line_items,  :user, :adjustments, :number, :email,
                             :total, :item_total, :ship_total, :adjustment_total, :state, :tax_total,
                             :special_instructions]
 
@@ -14,6 +14,14 @@ class Spree::OrderDrop < Clot::BaseDrop
 
   def display_item_total
     @source.display_item_total
+  end
+
+  def bill_address
+    @source.bill_address
+  end
+
+  def ship_address
+    @source.ship_address
   end
 
 end
