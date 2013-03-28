7ac09a7448841c3b02dc753f8e5b5326813b0897
Alexander Negoda <alexander.negoda@gmail.com>
2013-03-28 19:39:36 +0400

    update the assets.precompile

--- config/initializers/liquid_engine.rb
+++ config/initializers/liquid_engine.rb
@@ -3,6 +3,8 @@ ActionView::Template.register_template_handler :liquid, ActionView::Template::Ha
 
 #ActionController::Base.prepend_view_path  Refinery::Themes::Theme.theme_path.join("views")
 
+Rails.application.config.assets.precompile += [/(^[^_\/]|\/[^_])[^\/]*$/] #[ /\A[^\/\\]+\.(css|js)$/i ]
+
 Rails.application.config.assets.paths.prepend Refinery::Themes::Theme.theme_path.join("assets/javascripts").to_s
 Rails.application.config.assets.paths.prepend Refinery::Themes::Theme.theme_path.join("assets/stylesheets").to_s
 Rails.application.config.assets.paths.prepend Refinery::Themes::Theme.theme_path.join("assets/images").to_s
